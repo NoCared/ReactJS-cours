af7193edd9755dc5a5e2712b48c76e3c1774ab64
nocared <paul.richarddu30@gmail.com>
2023-10-26 16:28:53 +0200

    ADD  - Syntaxe

--- Syntaxe/assets/js/script.js
+++ Syntaxe/assets/js/script.js
@@ -30,17 +30,50 @@ root.render(nav);
 const menuEntries = ["Logo", "About", "Contact"];
 //  com javascript
 const nav2 = <nav>
-    <ul>
+    <ul className="menu">
         {/* ceci est un commentaire JSX,
             pas de foreach mais à la place map()
             Pour retourner à nouveau du JSX dans une callback
             je dois utiliser un return
         */}
         {
-            menuEntries.map((element,index) => {
-                return (<li key={index}>{element}</li>);
+            menuEntries.map((element, index) => {
+                return (<li key={index} className="element">{element}</li>);
             })
         }
     </ul>
 </nav>
 root.render(nav2);
+
+// je peux créer sous forme de fonction des components
+// chaque component doit s'écrire avec une capitale
+const Card = () => {
+    return (<div className="card">
+        <img src="./assets/img/test.jp"></img>
+        <p>lorem</p>
+    </div>)
+}
+root.render(<Card></Card>);
+
+//
+class Footer extends React.Component {
+    menuEntries = [];
+    render() {
+        return (
+            <> {/*Je ne peux pas utiliser d'elements siblings
+                à la racine d'un component mais uniquement des
+                éléments imbriqués.
+                Par contre je peux utiliser des éléments vides comme
+                parents : <></>
+            */}
+                <div>
+                    mes partenaires
+                </div>
+                <footer>
+                    <p>footer</p>
+                </footer>
+            </>
+        )
+    }
+}
+root.render(<Footer></Footer>);

dee8af3f2fa51f3d125aec7dfce382ff674923c4
nocared <paul.richarddu30@gmail.com>
2023-11-13 09:35:12 +0100

    ADD - Start creation menu

---
+++ menuReact/assets/Menu/menuEntries.js
@@ -0,0 +1,14 @@
+const menuEntries = [
+    {
+        "label" : "item 1",
+        "link" : null
+    },
+    {
+        "label" : "item 2",
+        "link" : null
+    },
+    {
+        "label" : "item 3",
+        "link" : null
+    }
+]

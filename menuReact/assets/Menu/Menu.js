a8f5695b2088bd62df34afc12826a73c3fbe022a
nocared <paul.richarddu30@gmail.com>
2023-11-13 11:03:57 +0100

    ADD - Menu display

--- menuReact/assets/Menu/Menu.js
+++ menuReact/assets/Menu/Menu.js
@@ -1,3 +1,41 @@
-class Menu {
 
+// class Menu extends React.Component {
+//     render(){
+//         return(
+//             <nav>
+//                 <div className="logo">
+//                     Mon Logo
+//                 </div>
+
+//                 <ul>
+//                     {
+//                         menuEntries.map((value,index)=>{
+//                         return<li key={index}>{value.label}</li>
+//                         })
+//                     }
+//                 </ul>
+//             </nav>
+//         );
+//     }
+// }
+
+const Menu = () => {
+    return (
+        <nav>
+            <div className="logo">
+                Mon Logo
+            </div>
+
+            <ul>
+                {
+                    menuEntries.map((value, index) => {
+                        return <li key={index}>{value.label}</li>
+                    })
+                }
+            </ul>
+        </nav>)
 }
+
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<Menu></Menu>);
